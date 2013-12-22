c929f08269f4082a3e77bf47d8bd6029a6baedf1
camelmasa <camelmasa@gmail.com>
2013-12-23 08:42:33 +0900

    Change user routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Connect::Application.routes.draw do
   resources :wikis, path: 'wiki'
 
-  devise_for :users
+  devise_for :users, path: 'user'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

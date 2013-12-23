2363942b3e18351209f7fe050440d48102c956db
camelmasa <camelmasa@gmail.com>
2013-12-23 09:15:52 +0900

    Does not show wiki page to logged out user

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,4 +1,5 @@
 class WikisController < ApplicationController
+  before_action :render_404, unless: :user_signed_in?
   before_action :set_wiki, only: [:show, :edit, :update, :destroy]
 
   # GET /wikis

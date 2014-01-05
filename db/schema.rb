07af3fd286ceae0e5bd478798552881fdf633ca2
camelmasa <camelmasa@gmail.com>
2014-01-05 11:39:06 +0900

    Add raw_body column to wikis table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131222193800) do
+ActiveRecord::Schema.define(version: 20140105023734) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20131222193800) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "raw_body"
   end
 
 end

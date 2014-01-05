9c6864edd5c453a38f96a8911cd1eac97cc281ba
camelmasa <camelmasa@gmail.com>
2014-01-05 15:36:18 +0900

    Modify TextConvertObserver to ConvertMarkdownToHtmlObserver

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Connect
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.active_record.observers = :text_convert_observer
+    config.active_record.observers = [
+      :convert_markdown_to_html_observer
+    ]
   end
 end

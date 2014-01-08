ee41ee08bf9f35cf710add3535610f46f75d0d90
camelmasa <camelmasa@gmail.com>
2014-01-08 16:52:11 +0900

    Add username column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108070232) do
+ActiveRecord::Schema.define(version: 20140108074506) do
 
   create_table "comments", force: true do |t|
     t.integer  "blog_id"
@@ -55,12 +55,14 @@ ActiveRecord::Schema.define(version: 20140108070232) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.string   "role"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "versions", force: true do |t|
     t.string   "item_type",  null: false

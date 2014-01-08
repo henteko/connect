9eccb4a3208f277fe800a06c7a7ad57f883bb6e1
camelmasa <camelmasa@gmail.com>
2014-01-08 15:15:36 +0900

    Fix broken test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe HomeController do
   describe '#index' do
-    it 'returns 200' do
+    it 'returns 302' do
       get :index
-      expect(response.status).to eq 200
+      expect(response.status).to eq 302
+    end
+    context 'with sign in' do
+      let(:user) { create :user }
+      before { sign_in user }
+      it 'returns 200' do
+        get :index
+        expect(response.status).to eq 200
+      end
     end
   end
 end

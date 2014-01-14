ae63d6b5734de9da56bd835d0b230942f8e9d54d
camelmasa <camelmasa@gmail.com>
2014-01-14 20:04:47 +0900

    Refactor ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :manage,  :all if user.role == 'admin'
-    can :edit,    Blog, user: user
-    can :update,  Blog, user: user
-    can :destroy, Blog, user: user
+    can [:edit, :update, :destroy], Blog, user: user
   end
 end

c3a4f33bd43de1f49bb613e489200662bb5ceec9
camelmasa <camelmasa@gmail.com>
2014-01-16 21:57:19 +0900

    Update Hipchat model

---
+++ app/models/hipchat.rb
@@ -0,0 +1,13 @@
+class Hipchat < Notification
+  def notify(object)
+    message = object.versions.count == 1 ? 'Created ' : 'Updated '
+    message << object.class.to_s.downcase
+
+    # TODO: Add link to object page url
+    # message << " <a href='#{object.url}'>#{object.title}</a>"
+    #
+    client = HipChat::Client.new(token)
+    client[room_name].send('Connect', message, notify: false,
+                           message_format: 'html')
+  end
+end

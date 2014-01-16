f71b3a9432fa4652884078784b7774d97640bb71
camelmasa <camelmasa@gmail.com>
2014-01-16 21:58:41 +0900

    Update notify_to_service_observer

--- app/models/observers/notify_to_service_observer.rb
+++ app/models/observers/notify_to_service_observer.rb
@@ -3,6 +3,6 @@ class NotifyToServiceObserver < ActiveRecord::Observer
   observe :page, :blog, :comment
 
   def after_save(record)
-    Services::Hipchat.notify(record) if ENV['HIPCHAT_NOTIFY_ACTIVE']
+    Notification.notify(record)
   end
 end

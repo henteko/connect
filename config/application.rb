09440cb69db396cb3f050c5f38b15e8cad413ac2
camelmasa <camelmasa@gmail.com>
2014-01-17 23:17:38 +0900

    Change config.i18n.default_locale = :ja

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,11 @@ module Connect
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
+    # TODO: multiple locale
+    config.i18n.default_locale = :ja
+
     config.active_record.observers = [
       :convert_markdown_to_html_observer,
       :convert_emoji_observer,

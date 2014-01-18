5c7c098fb4aff4716bf8414c6019371d83c98734
camelmasa <camelmasa@gmail.com>
2014-01-18 16:49:15 +0900

    Add action_mailer setting for SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ Connect::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # config.action_mailer.default_url_options = { host: 'please-change-me@connect.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.sendgrid.net',
+  #   port:                 587,
+  #   user_name:            ENV['SENDGRID_USERNAME'],
+  #   password:             ENV['SENDGRID_PASSWORD'] }
+
 end

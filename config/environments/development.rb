34cdfbb9306ac9eff0f9baed7d84721444e1dd37
henteko <henteko07@gmail.com>
2014-01-21 20:10:06 +0900

    add gmail smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,13 @@ Connect::Application.configure do
   config.assets.debug = true
 
   # For devise gem
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD']
+  }
 end
